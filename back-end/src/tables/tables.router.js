6d4af3eadf5a7fbd6a5935f2c072da5f04fd285c
William Janelli <williamjanelli@yahoo.com>
2024-02-01 08:05:04 -0800

    commentsadded

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,23 +1,23 @@
 /**
  * Defines the router for table resources.
- *
  * @type {Router}
  */
+const router = require("express").Router(); // Importing the Express router
+const methodNotAllowed = require("../errors/methodNotAllowed"); // Importing methodNotAllowed error handler
+const controller = require("./tables.controller"); // Importing the controller for table resources
 
-const router = require("express").Router();
-const methodNotAllowed = require("../errors/methodNotAllowed");
-const controller = require("./tables.controller");
-
+// Defining routes for the table resources
 router
   .route("/")
-  .get(controller.list)
-  .post(controller.create)
-  .all(methodNotAllowed);
+  .get(controller.list) // GET request handler to list tables
+  .post(controller.create) // POST request handler to create a new table
+  .all(methodNotAllowed); // Catch-all handler for unsupported HTTP methods
+
 router
   .route("/:table_id/seat")
-  .put(controller.update)
-  .get(controller.read)
-  .delete(controller.destroy)
-  .all(methodNotAllowed);
+  .put(controller.update) // PUT request handler to update a table's seat
+  .get(controller.read) // GET request handler to read table details
+  .delete(controller.destroy) // DELETE request handler to delete a table
+  .all(methodNotAllowed); // Catch-all handler for unsupported HTTP methods
 
-module.exports = router;
+module.exports = router; // Exporting the router
--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -17,7 +17,7 @@ function Menu() {
           to="/"
         >
           <div className="sidebar-brand-text mx-3">
-            <span>Reservation Menu</span>
+            <span>Periodic Tables</span>
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
